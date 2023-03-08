34185c5346bcfb86fe751f1a2961c7b929783a79
Fisayo Fasuyi <fisayofasuyi@yahoo.com>
2023-03-08 16:51:53 +0000

    Instrument Honeycomb

--- backend-flask/app.py
+++ backend-flask/app.py
@@ -3,6 +3,21 @@ from flask import request
 from flask_cors import CORS, cross_origin
 import os
 
+#observability tool
+from opentelemetry import trace
+from opentelemetry.instrumentation.flask import FlaskInstrumentor
+from opentelemetry.instrumentation.requests import RequestsInstrumentor
+from opentelemetry.exporter.otlp.proto.http.trace_exporter import OTLPSpanExporter
+from opentelemetry.sdk.trace import TracerProvider
+from opentelemetry.sdk.trace.export import BatchSpanProcessor
+
+# Initialize tracing and an exporter that can send data to Honeycomb
+provider = TracerProvider()
+processor = BatchSpanProcessor(OTLPSpanExporter())
+provider.add_span_processor(processor)
+trace.set_tracer_provider(provider)
+tracer = trace.get_tracer(__name__)
+
 from services.home_activities import *
 from services.notifications_activities import *
 from services.user_activities import *
@@ -15,6 +30,12 @@ from services.create_message import *
 from services.show_activity import *
 
 app = Flask(__name__)
+
+#Honeycomb
+FlaskInstrumentor().instrument_app(app)
+RequestsInstrumentor().instrument()
+
+
 frontend = os.getenv('FRONTEND_URL')
 backend = os.getenv('BACKEND_URL')
 origins = [frontend, backend]
