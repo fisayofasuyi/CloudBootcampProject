9f87c49102641b2ba573e5759dbbd875579d7bdd
Fisayo Fasuyi <fisayofasuyi@yahoo.com>
2023-03-15 23:04:00 +0000

    commited

--- frontend-react-js/src/App.js
+++ frontend-react-js/src/App.js
@@ -10,11 +10,30 @@ import MessageGroupPage from './pages/MessageGroupPage';
 import ConfirmationPage from './pages/ConfirmationPage';
 import React from 'react';
 import NotificationsFeedPage from './pages/NotificationsFeedPage';
+import {Amplify} from 'aws-amplify'
+
 import process from 'process';
 import {
   createBrowserRouter,
   RouterProvider
 } from "react-router-dom";
+
+//add amplify for decentralized authorisation
+Amplify.configure({
+  "AWS_PROJECT_REGION": process.env.REACT_APP_AWS_PROJECT_REGION,
+  "aws_cognito_region": process.env.REACT_APP_AWS_COGNITO_REGION,
+  "aws_user_pools_id": process.env.REACT_APP_AWS_USER_POOLS_ID,
+  "aws_user_pools_web_client_id": process.env.REACT_APP_CLIENT_ID,
+  "oauth": {} ,
+  Auth: {
+    region: process.env.REACT_APP_AWS_PROJECT_REGION,
+    userPoolId: process.env.REACT_APP_AWS_USER_POOLS_ID,
+    userPoolWebClientId: process.env.REACT_APP_CLIENT_ID
+
+  }
+})
+
+
 
 const router = createBrowserRouter([
   {
