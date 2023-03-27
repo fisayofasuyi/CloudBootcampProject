f5801fab7919f966338a9f012573d30094723ffd
Fisayo <fasuyifisayo@gmail.com>
2023-03-28 00:09:35 +0100

    committed

--- backend-flask/lib/db.py
+++ backend-flask/lib/db.py
@@ -1,9 +1,31 @@
 from psycopg_pool import ConnectionPool
 import os
-import re
-import sys
-from flask import current_app as app
+#import re
+#import sys
+#from flask import current_app as app
 
+
+
+def query_wrap_object(template):
+    sql = f"""
+    (SELECT COALESCE(row_to_json(object_row),'{{}}'::json) FROM (
+    {template}
+    ) object_row);
+    """
+    return sql
+def query_wrap_array(template):
+    sql = f"""
+    (SELECT COALESCE(array_to_json(array_agg(row_to_json(array_row))),'[]'::json) FROM (
+    {template}
+    ) array_row);
+    """
+    return sql
+
+connection_url = os.getenv('CONNECTION_URL')
+pool = ConnectionPool(connection_url)
+
+
+"""
 class Db:
   def __init__(self):
     self.init_pool()
@@ -82,20 +104,7 @@ class Db:
           "{}"
         else:
           return json[0]
-  def query_wrap_object(self,template):
-    sql = f"""
-    (SELECT COALESCE(row_to_json(object_row),'{{}}'::json) FROM (
-    {template}
-    ) object_row);
-    """
-    return sql
-  def query_wrap_array(self,template):
-    sql = f"""
-    (SELECT COALESCE(array_to_json(array_agg(row_to_json(array_row))),'[]'::json) FROM (
-    {template}
-    ) array_row);
-    """
-    return sql
+  
   def print_sql_err(self,err):
     # get details about the exception
     err_type, err_obj, traceback = sys.exc_info()
@@ -112,3 +121,4 @@ class Db:
     print ("pgcode:", err.pgcode, "\n")
 
 db = Db()
+"""
--- backend-flask/services/home_activities.py
+++ backend-flask/services/home_activities.py
@@ -1,7 +1,7 @@
 from datetime import datetime, timedelta, timezone
 from opentelemetry import trace
 
-from lib.db import db
+from lib.db import query_wrap_array
 #tracer = trace.get_tracer("home.activities")
 
 class HomeActivities:
@@ -11,6 +11,17 @@ class HomeActivities:
       #span = trace.get_current_span()
       #now = datetime.now(timezone.utc).astimezone()
       #span.set_attribute("app.now", now.isoformat())
-      sql = db.template('activities', 'home')
-      results = db.query_array_json(sql)
+      sql = query_wrap_array("""SELECT * FROM activities
+      """)
+
+      print(sql)
+
+      with pool.connection() as conn:
+         with conn.cursor() as cur:
+            cur.execute(sql)
+            json = cur.fetchone()
+      print(json[0])
+      return json[0]
+         
+      
       return results
