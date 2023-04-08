6dc7c5e14612aee3964339caf0635200f215460c
Fisayo <fasuyifisayo@gmail.com>
2023-04-08 14:06:44 +0100

    implemented dynamodb for user and other messages

--- backend-flask/bin/db/seed.sql
+++ backend-flask/bin/db/seed.sql
@@ -1,6 +1,6 @@
-INSERT INTO public.users (display_name, handle, cognito_user_id) VALUES 
-('Fisayo Fasuyi', 'fisayofasuyi', 'MOCK'),
-('Fred Mann', 'fredmann', 'MOCK');
+INSERT INTO public.users (display_name, email, handle, cognito_user_id) VALUES 
+('Fisayo Fasuyi', 'fasuyifisayo@gmail.com','fisayofasuyi', 'MOCK'),
+('Fred Mann', 'fisayofasuyi@yahoo.com','fredmann', 'MOCK');
 
 INSERT INTO public.activities (user_uuid, message, expires_at)
 VALUES
