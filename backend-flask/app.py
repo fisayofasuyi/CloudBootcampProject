3b91faacc29b68d3e14f3b610d62ab9e2d24a44a
Fisayo Fasuyi <fisayofasuyi@yahoo.com>
2023-05-09 21:37:46 +0000

    committed

--- backend-flask/app.py
+++ backend-flask/app.py
@@ -330,5 +330,5 @@ if __name__ == "__main__":
   app.run(debug=True)
 
 if __name__ == '__main__':
-  app.run(host='0.0.0.0', port=8080)
-  #app.run(host='localhost', port=5000)
+  # app.run(host='0.0.0.0', port=8080)
+  app.run(host='localhost', port=5000)
